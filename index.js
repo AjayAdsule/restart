6d32b6170826058c3197c2cebbe0bca8babe9877
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2023-02-12 03:50:05 +0530

    x
    x

--- index.js
+++ index.js
@@ -28,11 +28,17 @@ class person{
         }
     }
 }
-let ajay=new person("ajay",1999,7977223876,2021)
+// let ajay=new person("ajay",1999,7977223876,2021)
 // ajay.result();
 
-let arr=["ajay","vijay",["mohit","rohit",12,85]]
-arr.push(["dinesh"])
-arr.pop()
-arr.sort()
-console.log(arr)
+// let arr=["ajay","vijay",["mohit","rohit",12,85]]
+// arr.push(["dinesh"])
+// arr.pop()
+// arr.sort()
+// console.log(arr)
+
+// create a function that will do async operation
+
+const time=()=>{
+    return new Date().getHours();
+}
