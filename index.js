42fb8bb8f0ba6530ad14f44e5d11333eab35ba43
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2023-02-26 04:04:08 +0530

    f
    f

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 // make async function
-let getdate=()=>{
-    return new Date().getHours()
-    +":"
-    + new Date().getMinutes()
-    +":"
-    + new Date().getSeconds()
-}
+// let getdate=()=>{
+//     return new Date().getHours()
+//     +":"
+//     + new Date().getMinutes()
+//     +":"
+//     + new Date().getSeconds()
+// }
 
-//learning async function
-async function whe(){
-    let mumbai=new Promise((reslove)=>{
-        setTimeout(() => {
-            reslove("21 deg")
-        }, 2000);
-    })
-    let delhi=new Promise((reslove)=>{
-        setTimeout(() => {
-            reslove("27 deg")
-        }, 5000);
-    })
-    console.log(`${getdate()} fethching mumbai whether......`)
-    let mumw= await mumbai
+// //learning async function
+// async function whe(){
+//     let mumbai=new Promise((reslove)=>{
+//         setTimeout(() => {
+//             reslove("21 deg")
+//         }, 2000);
+//     })
+//     let delhi=new Promise((reslove)=>{
+//         setTimeout(() => {
+//             reslove("27 deg")
+//         }, 5000);
+//     })
+//     console.log(`${getdate()} fethching mumbai whether......`)
+//     let mumw= await mumbai
     
-    console.log(`${getdate()} mumbai whether is ${mumw}`)
-    console.log("fethching delhi whether......")
-    let delw=await delhi
-    console.log(`${getdate()} delhi whether is ${delw}`)
+//     console.log(`${getdate()} mumbai whether is ${mumw}`)
+//     console.log("fethching delhi whether......")
+//     let delw=await delhi
+//     console.log(`${getdate()} delhi whether is ${delw}`)
 
-}
-console.log("feth data from whether department")
-whe()
+// }
+// console.log("feth data from whether department")
+// whe()
+
+let p= async new Promise((resolve, reject) =>{
+    setTimeout(() => {
+        resolve("21 deg")
+    },2000)
+    
+})
